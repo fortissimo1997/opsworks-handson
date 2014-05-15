924d6ec8f160b57e5b01ebacc19b7198342d64dc
fortissimo1997 <fortissimo1997@gmail.com>
2014-05-15 09:51:41 +0900

    raw.github.com was moved to raw.githubusercontent.com

--- wp-cli/recipes/default.rb
+++ wp-cli/recipes/default.rb
@@ -8,7 +8,7 @@ package 'mysql' do
 end
 
 execute 'wp-cli install' do
-  command "curl -L https://raw.github.com/wp-cli/builds/gh-pages/phar/wp-cli.phar > wp-cli.phar && chmod +x wp-cli.phar && cp wp-cli.phar #{wpdir}/wp"
+  command "curl -L https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar > wp-cli.phar && chmod +x wp-cli.phar && cp wp-cli.phar #{wpdir}/wp"
   cwd '/home/ec2-user'
   action :run
   not_if { ::File.exists?("#{wpdir}/wp") }
