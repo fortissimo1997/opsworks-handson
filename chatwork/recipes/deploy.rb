4be5516a255abcb3ae2d354be19ea91326686def
fortissimo1997 <fortissimo1997@gmail.com>
2014-05-15 09:11:18 +0900

    Add Chatwork

---
+++ chatwork/recipes/deploy.rb
@@ -0,0 +1,13 @@
+node[:deploy].each do |application, deploy|
+  token = deploy[:chatwork_token]
+  room  = deploy[:chatwork_room_id]
+  return true if token.nil? || room.nil?
+  return true if deploy[:scm].nil? || deploy[:deploying_user].nil?
+  user = deploy[:deploying_user].split('/')[1]
+  return true if deploy[:notifying_nodes].include?(node[:hostname])
+
+  execute 'post notification to chatwork' do
+    command "curl -X POST -H \"X-ChatWorkToken: #{token}\" -d \"body=Application+#{application}+deployed+by+#{user}\" \"https://api.chatwork.com/v1/rooms/#{room}/messages\""
+    action :run
+  end
+end
